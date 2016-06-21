9bb98b0397e2cc703fde57235252951e87d3fdad
VJSbox <vsahakian@usgs.gov>
2016-06-21 15:57:16 -0700

    jun21_1557_lbox

--- src/gmpe.py
+++ src/gmpe.py
@@ -1,9 +1,9 @@
-#GMPE
+######GMPE######
 #Module for computing predicted ground motion given coefficents
 #VJS 6/2016
 
 
-def ask2014(db,coeff_file,M1,M2,mdep_ffdf):
+def ask2014_pga(db,coeff_file,M1,M2,mdep_ffdf):
     '''
     Compute the predicted ground motionsfor a given set of events using the
     Abrahamson, Silva, and Kamai 2014 model.
@@ -18,4 +18,58 @@ def ask2014(db,coeff_file,M1,M2,mdep_ffdf):
         PGA:            Predicted PGA
     '''
     
+    from numpy import genfromtxt
+    
+    #Read in coefficients file:
+    ask2014=genfromtxt(coeff_file,skip_header=1)
+    
+    #Get the PGA coefficients:
+    #Coefficients for median ground motions
+    T=ask2014[:,0]
+    M1=ask2014[:,1]
+    vlin=ask2014[:,2]
+    b=ask2014[:,3]
+    c=ask2014[:,4]
+    c4=ask2014[:,5]
+    a1=ask2014[:,6]
+    a2=ask2014[:,7]
+    a3=ask2014[:,8]
+    a4=ask2014[:,9]
+    a5=ask2014[:,10]
+    a6=ask2014[:,11]
+    a8=ask2014[:,12]
+    a10=ask2014[:,13]
+    a11=ask2014[:,14]
+    a12=ask2014[:,15]
+    a13=ask2014[:,16]
+    a14=ask2014[:,17]
+    a15=ask2014[:,18]
+    a17=ask2014[:,19]
+    a43=ask2014[:,20]
+    a44=ask2014[:,21]
+    a45=ask2014[:,22]
+    a46=ask2014[:,23]
+    
+    #Coefficients for the median ground motion in other regions
+    a25=ask2014[:,24]
+    a28=ask2014[:,25]
+    a29=ask2014[:,26]
+    a31=ask2014[:,27]
+    a36=ask2014[:,28]
+    a37=ask2014[:,29]
+    a38=ask2014[:,30]
+    a39=ask2014[:,31]
+    a40=ask2014[:,32]
+    a41=ask2014[:,33]
+    a42=ask2014[:,34]
+    
+    #Coefficients for hte standard deviation
+    s1e=
+    s2e=
+    s3=
+    s4=
+    s1m=
+    s2m=
+    s5=
+    s6=
     
