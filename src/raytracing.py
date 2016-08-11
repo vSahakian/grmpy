e6c3ee5426ee766992e5d9a4bc231db10717fa63
vsahakian <sahakian.valerie@gmail.com>
2016-08-10 17:36:30 -0700

    raytracing now writes source.in and receiver.in files correctly

--- src/raytracing.py
+++ src/raytracing.py
@@ -11,7 +11,7 @@ def write_sourcein(home,run_name,veltype):
     Input:
         home:           String to home path for the main working directory
         run_name:       String to the name of the run
-        veltype:        0=Vp, 1=Vs
+        veltype:        1=Vp, 2=Vs
     Output:
         source_veltype.in:     Writes out the source.in file for raytracing
     '''
@@ -22,13 +22,15 @@ def write_sourcein(home,run_name,veltype):
     from numpy import str
     
     #Get the velocity type:
-    if veltype==0:
+    if veltype==1:
         vm='Vp'
-    elif veltype==1:
+    elif veltype==2:
         vm='Vs'
     
     #Source is always local, for now:
     telesource=0
+    #Number of paths always 1, since there's just one layer:
+    npaths=1
     
     #Get the path to the run directory:
     run_dir=path.expanduser(home+run_name+'/')
@@ -64,8 +66,7 @@ def write_sourcein(home,run_name,veltype):
         sdepth=eventi.edepth[0]
         slat=eventi.elat[0]
         slon=eventi.elon[0]
-        #number of paths
-        npaths=len(eventi.sta)
+        #number of paths - always =1 here, defined above
         
         #Number of sections on the path:  Always 1 here, since one layer
         nsections=1
@@ -95,7 +96,7 @@ def write_sourcein(home,run_name,veltype):
     print 'Wrote file '+sourcefile+' for velocity type '+vm
 
 
-def write_receiverin():
+def write_receiverin(home,run_name):
     '''
     Write a receiver.in in file for fm3d raytracing
     Input:
@@ -108,19 +109,91 @@ def write_receiverin():
     from os import path
     import cPickle as pickle
     import dread
-    from numpy import str
+    from numpy import str,ones,int64
     
     #Get the path to the run directory:
     run_dir=path.expanduser(home+run_name+'/')
     #Get the event object directory:
-    sodir=run_dir+'event_objs/'
+    sodir=run_dir+'sta_objs/'
     #Get the path to the list of event objects:
     station_list_path=sodir+run_name+'.pckl'
     
     #Also get the path for the output sources.in file:
     raydir=run_dir+'raytracing/'
-    sourcefile=raydir+'receivers.in'
+    receiverfile=raydir+'receivers.in'
     
+    ##
+    #Read in the list of station objects:
+    sobjs=dread.read_obj_list(station_list_path)
+    
+    #Open the receiver file to write:
+    f=open(receiverfile,'w')
+    
+    #Get the number of receivers:
+    nrec=len(sobjs)
+    #Get the line to write:
+    nrecline=str(nrec)+'\t\t\t\t number of receivers\n'
+    #Write line
+    f.write(nrecline)
+    
+    #For each station, write the receiver information:
+    for rec_ind in range(len(sobjs)):
+        receiveri=sobjs[rec_ind]
+        
+        #Get info:
+        #station info
+        #set depth to 0, meaning it's on the surface interface (which has elevation in model)
+        rdepth=0
+        #get lat/lon
+        rlat=receiveri.stlat[0][0]
+        rlon=receiveri.stlon[0][0]
+        #number of paths to receiver
+        npaths=len(receiveri.source_i)
+        #The source of each path:
+        sourcei=receiveri.source_i
+        #The number of the source for each path - here it's always 1, since 
+        #we're just raytracing through one layer...make them integers:
+        npsource=ones((len(sourcei)))
+        npsource=npsource.astype(int64)
+        
+        #Turn them into printable lines:
+        rposline=str(rdepth)+' '+str(rlat)+' '+str(rlon)+'\t\t position depth(km),lat(deg),lon(deg)\n'
+        nposline=str(npaths)+'\t\t\t\t number of paths to this receiver\n'
+        
+        #Turn the arrays into printable lines:
+        #First the source number:
+        for source in range(len(sourcei)):
+            if source==0:
+                sourceiline=str(sourcei[source][0])
+            else:
+                sourceiline=sourceiline+' '+str(sourcei[source][0])
+        #end it:
+        sourceiline=sourceiline+'\t\t the source of each path\n'
+        
+        #Now the number of the path in the source list:
+        for path in range(len(npsource)):
+            if path==0:
+                npsourceline=str(npsource[path])
+            else:
+                npsourceline=npsourceline+' '+str(npsource[path])
+        #end it:
+        npsourceline=npsourceline+'\t\t\t number of the path in the source list\n'
+        
+        print sourceiline
+        
+        #Write them:
+        f.write(rposline)
+        f.write(nposline)
+        f.write(sourceiline)
+        f.write(npsourceline)
+        
+    #Close the file:
+    f.close()
+    
+    #Print statememnt:
+    print 'Wrote file '+receiverfile
+    
+        
     
     
 ###################################
