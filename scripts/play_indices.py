f353ff971f692aa20fe4de1a04bfad6e02b9e82c
vsahakian <sahakian.valerie@gmail.com>
2016-09-26 21:50:19 -0700

    Adding interpolation of velocity on raypath

--- scripts/play_indices.py
+++ scripts/play_indices.py
@@ -3,10 +3,38 @@
 
 import dread
 import cdefs as cdf
+import cPickle as pickle
+from numpy import meshgrid,zeros
+from scipy.interpolate import griddata,Rbf
+
+#Change this parameter depending on where you run:
+#0=desktop
+#1=mac
+
+what_home=1
+
+if what_home==0:
+    #Desktop:
+    HOME='/media/vsahakian/katmai'
+elif what_home==1:
+    #Mac:
+    HOME='/Users/vsahakian'
+    
 
 #Paths to Fang 2016 model
-coordspath='/media/vsahakian/katmai/anza/data/vm/Fang2016/coords.txt'
-materialmodelpath='/media/vsahakian/katmai/anza/data/vm/Fang2016/Vs.dat'
+coordspath=HOME+'/anza/data/vm/Fang2016/coords.txt'
+materialmodelpath=HOME+'/anza/data/vm/Fang2016/Vs.dat'
+
+#Path to residuals object:
+rpath=HOME+'/anza/models/residuals/abdb_5sta_0-6.5_topography/abdb_5sta_0-6.5_topography_robj_raydat.pckl'
+
+########
+#######
+
+#Read in residuals object
+rfile=open(rpath,'r')
+robj=pickle.load(rfile)
+rfile.close()
 
 #Read in data:
 x,y,z,nx,ny,nz,mod=dread.read_material_model(coordspath,materialmodelpath)
@@ -15,9 +43,39 @@ x,y,z,nx,ny,nz,mod=dread.read_material_model(coordspath,materialmodelpath)
 x=x-360
 
 #Make object:
-materialmodel=cdf.material_model(x,y,z,nx,ny,nz,mod)
+mmodel=cdf.material_model(x,y,z,nx,ny,nz,mod)
 
 
 ####Test for the first ray:#######
 #Get actual values of x, y, and z:
-grid_x=
+grid_x=mmodel.x
+grid_y=mmodel.y
+grid_z=-mmodel.z
+
+#Get the values to interpolate (aka values of ray...for now just one ray):
+ray_x=robj.vs_lon[0]
+ray_y=robj.vs_lat[0]
+ray_z=robj.vs_depth[0]
+
+
+##
+#Make points for griddata:
+gX,gY,gZ=meshgrid(grid_x,grid_y,grid_z,indexing='ij')
+
+#Make column vectors
+columnx=gX.ravel()
+columny=gY.ravel()
+columnz=gZ.ravel()
+data=mmodel.materials.transpose(2,1,0).ravel()
+
+#Make interpolator
+interpolator = Rbf(columnx, columny, columnz, data,function='linear')
+
+#Interpolate
+vs=interpolator(ray_x,ray_y,ray_z)
+
+##Scatter ray:
+scatter(ray_x,ray_z,c=vs,s=60,lw=0.1)
+
+#Scatter interpolater velocity model:
+scatter(columnx[i],columny[i],c=data[i],s=70)
