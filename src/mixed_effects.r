9dba98d0c3015ec368de432aca69ed4911c838cd
vsahakian <sahakian.valerie@gmail.com>
2016-11-07 13:25:39 -0800

    almost done with r function

--- src/mixed_effects.r
+++ src/mixed_effects.r
@@ -3,22 +3,94 @@
 ##############################################
 
 
-### Put Get coefficients functions here ###
-#############
-#####
+############## Functions needed for mixed effects ##############
 
+# Method to get fixed effects
+
+get_fixed <- function(model, ...){ UseMethod("get_fixed") }
+
+get_fixed.merMod <- function(model, ...){
+	require(lme4)
+	# get a data.frame of the fixed effects
+	coef.tbl <- {
+		modsum <- summary(model) #Can also use 'fixef' but this doesn't return std errors
+		coefs <- modsum[['coefficients']]
+		colnames(coefs) <- c('Estimate','Std.error','t.value')
+		coefs <- cbind(data.frame(fixed.effect=rownames(coefs)), coefs)
+		rownames(coefs) <- NULL
+		return(coefs)
+	}
+	return(coef.tbl)
+}
+
+####################################################################
+
+# Method to get the site random effects
+
+get_site <- function(model, ...){ UseMethod("get_site") }
+
+get_site.merMod <- function(model, ...){
+	require(lme4)
+	# get a data.frame of the random site effects
+	modran <- lme4::ranef(model, condVar=TRUE)
+	# internal function to add the std errors to the output
+	.add_stderr <- function(x){
+		id <- rownames(x)
+		bias <- x$`(Intercept)`
+		vars <- as.vector(attr(x, 'postVar'))
+		std.err <- sqrt(vars)
+		return(data.frame(ID=id, Bias=bias, Std.error=std.err))
+	}
+	
+	coefs <- .add_stderr(modran[['sta']])
+	return(coefs)
+	}
+	
+####################################################################
+
+# Method to get the event random effects
+
+get_event <- function(model, ...){ UseMethod("get_event") }
+
+get_event.merMod <- function(model, ...){
+	require(lme4)
+	# get a data.frame of the random event effects
+	modran <- lme4::ranef(model, condVar=TRUE)
+	# internal function to add the std errors to the output
+	.add_stderr <- function(x){
+		id <- rownames(x)
+		bias <- x$`(Intercept)`
+		vars <- as.vector(attr(x, 'postVar'))
+		std.err <- sqrt(vars)
+		return(data.frame(ID=id, Bias=bias, Std.error=std.err))
+	}
+	
+	coefs <- .add_stderr(modran[['evnum']])
+	return(coefs)
+	}
+
+	
+
+###############################################################################
+###############################################################################
 
 
 ### Run Mixed Effects ###
-r_mixedeffects <- function(datacsv, ...) {
+r_mixedeffects <- function(datacsv, home, database...) {
+	
+	# Input:
+	#		datacsv: 		Full path to the csv file written by python
+	#		home: 			String with path to home (i.e., /Users/vsahakian/ or /home/vsahakian/katmai)
+	# 		database: 		String with database name (i.e., "test2013", or "abdb"
+	#
 	
 	## Libraries:
-	library(lme4)
+	require(lme4)
 
 	## Read in file:
 	db <- read.table(datacsv)
 	
-	## Set the V1 and V6 columns, columns of station names and event numbers, as a factor
+	## Set the evnum and sta columns, columns of station names and event numbers, as a factor
 	db$evnum <- as.factor(db$evnum)
 	db$sta <- as.factor(db$sta)
 	
@@ -29,23 +101,33 @@ r_mixedeffects <- function(datacsv, ...) {
 	## Run Models ##
 	##			  ##
 	
-	model <- lmer(pga ~ m + m2 + lnR + rrup + (1|evnum) + (1|sta), data=db)
+	model <- lme4::lmer(pga ~ m + m2 + lnR + rrup + (1|evnum) + (1|sta), data=db)
 	
 	## Print to screen:
 	print(model)
 	
 	## Also save coefficients to a csv file, ##NAME##
-	fixed_coefs <- get_coefs(
-**********	filename <- file.path(".","my_results_random.csv")
+	fixed_coefs <- get_coefs(model)
+    filename <- file.path(home,"anza/models/pckl",database,"results_fixed.csv")
 	readr::write_csv(fixed_coefs, path=filename)
 	message("Fixed coefficients saved to: ", filename)
 	
-	site_coefs <- get_site(
 	
-	event_coefs <- get_event(
+	site_coefs <- get_site(model)
+	filename <- file.path(home,"anza/models/pckl",database,"results_site.csv")
+	readr::write_csv(site_coefs, path=filename)
+	message("Site effects saved to: ", filename)
+	
+	
+	event_coefs <- get_event(model)
+	filename <- file.path(home,"anza/models/pckl",database,"results_event.csv")
+	readr::write_csv(event_coefs, path=filename)
+	message("Event effects saved to: ", filename)
+	
 	
 	
 ### Call Function ###
 
 print("Running mixed effects model...")
 
+r_mixedeffects(
