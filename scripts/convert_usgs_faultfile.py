fc5bc1d8fc4672584c9bc6e1f2794bc73505b466
vsahakian <sahakian.valerie@gmail.com>
2016-12-05 09:14:08 -0800

    faultfile script changed

--- scripts/convert_usgs_faultfile.py
+++ scripts/convert_usgs_faultfile.py
@@ -7,10 +7,11 @@ import dread
 multisegpath='/Users/vsahakian/anza/data/faults/Holocene_LatestPleistocene.txt'
 
 #Name of the pckl output file:
-pcklpath='/Users/vsahakian/anza/data/faults/Holocene_LatestPleistocene_117.5w_115.5w_33n_34n.txt'
+#pcklpath='/Users/vsahakian/anza/data/faults/Holocene_LatestPleistocene_117.5w_115.5w_33n_34n.txt'
+pcklpath='/Users/vsahakian/anza/data/faults/Holocene_LatestPleistocene_118.0w_115.2w_32.3n_34.5n.txt'
 
 #Limits to output:
-pathlimits=[[-117.5,-115.5],[33,34]]
+pathlimits=[[-118.0,-115.2],[32.3,34.5]]
 
 #Convert:
 allsegments=dread.multiseg2pckl(multisegpath,pcklpath,pathlimits)
