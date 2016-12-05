2b1bb7101e88852d4af7775380b283a069a06743
vsahakian <sahakian.valerie@gmail.com>
2016-12-05 09:24:03 -0800

    changing faultfile again

--- scripts/convert_usgs_faultfile.py
+++ scripts/convert_usgs_faultfile.py
@@ -9,6 +9,8 @@ multisegpath='/Users/vsahakian/anza/data/faults/Holocene_LatestPleistocene.txt'
 #Name of the pckl output file:
 #pcklpath='/Users/vsahakian/anza/data/faults/Holocene_LatestPleistocene_117.5w_115.5w_33n_34n.txt'
 pcklpath='/Users/vsahakian/anza/data/faults/Holocene_LatestPleistocene_118.0w_115.2w_32.3n_34.5n.txt'
+pcklpath='/Users/vsahakian/anza/data/faults/Holocene_LatestPleistocene_118.0w_115.2w_32.3n_34.5n.pckl'
+
 
 #Limits to output:
 pathlimits=[[-118.0,-115.2],[32.3,34.5]]
