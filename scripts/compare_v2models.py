fixed a sign term in iterative mixed effects, made plotting script to compare them all

scripts/compare_v2models.py
@@ -4,11 +4,15 @@
 import cPickle as pickle
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import pearsonr
 
 
 #fig dir:
 png_dir = '/Users/vsahakian/anza/models/residuals/sm_me_iterative_compare/figs/'
 pdf_dir = '/Users/vsahakian/anza/models/residuals/sm_me_iterative_compare/figs/pdf/' 
+
+# Decimation factor for event plotting:
+dfactor = 50
 
 ## Read in models: 
 
@@ -27,7 +31,7 @@ me6cff = pickle.load(modfile)
 modfile.close()
 
 # Model 3, 6 coefficients, iterative ME intercept
-me6cff_iter_path = '/Users/vsahakian/anza/models/residuals/mixedregr_v2anza2013_pga_6coeff_a2_0.73_a3_-0.14_a4_-1.68_a5_-0.01_a6_1.03_Mc_8.5_res4/mixedcoeff_v2anza2013_pga_vs30_6coeff_pga__ncoeff6_Mc_8.5_VR_81.4_a2_0.73_a3_-0.14_a4_-1.68_a5_-0.01_a6_1.03_robj.pckl'
+me6cff_iter_path = '/Users/vsahakian/anza/models/residuals/mixedregr_v2anza2013_pga_6coeff_a2_0.73_a3_-0.14_a4_-1.68_a5_-0.01_a6_1.03_Mc_8.5_res4/mixedcoeff_v2anza2013_pga_vs30_6coeff_pga__ncoeff6_Mc_8.5_VR_84.3_a2_0.73_a3_-0.14_a4_-1.68_a5_-0.01_a6_1.03_robj.pckl'
 
 modfile = open(me6cff_iter_path,'r')
 me6cff_iter = pickle.load(modfile)
@@ -104,7 +108,7 @@ plt.errorbar(sm5cff.stnum[usta_ind],np.array(sm5cff.site_terms)[usta_ind],yerr=n
 plt.errorbar(me5cff.stnum[usta_ind],np.array(me5cff.site_terms)[usta_ind],yerr=np.array(me5cff.site_stderr)[usta_ind],fmt='D',markersize=8,color='#796899',elinewidth=2,capsize=4,capthick=1.5,label='Mixed 5 coefficients')
 
 #m e5cff iterative
-plt.errorbar(me5cff_iter.stnum[usta_ind],np.array(me5cff_iter.site_terms)[usta_ind],yerr=np.array(me5cff_iter.site_stderr)[usta_ind],fmt='p',markersize=8,color='#d17c21',elinewidth=2,capsize=4,capthick=1.5,label='Iterative-Mixed 5 coefficients')
+plt.errorbar(me5cff_iter.stnum[usta_ind],np.array(me5cff_iter.site_terms)[usta_ind],yerr=np.array(me5cff_iter.site_stderr)[usta_ind],fmt='p',markersize=8,color='#24822f',elinewidth=2,capsize=4,capthick=1.5,label='Iterative-Mixed 5 coefficients')
 
 
 # Station name text:
@@ -117,6 +121,9 @@ for stationi in range(len(usta)):
     plt.text(stx,sty,statext,rotation=rotangle,ha='center',va='bottom')
 
 plt.legend()
+
+plt.xlabel('Station Number and Name')
+plt.ylabel('Site Term (ln residual)')
 plt.title('Site vs. Site Terms, all runs')
 
 plt.savefig(allsitefig_png)
@@ -151,6 +158,9 @@ for stationi in range(len(usta)):
 
 
 plt.legend()
+
+plt.xlabel('Station Number and Name')
+plt.ylabel('Site Term (ln residual)')
 plt.title('Site vs. Site terms, 6 coefficients/with Vs30 term only')
 
 plt.savefig(c6sitefig_png)
@@ -172,7 +182,7 @@ plt.errorbar(sm5cff.stnum[usta_ind],np.array(sm5cff.site_terms)[usta_ind],yerr=n
 plt.errorbar(me5cff.stnum[usta_ind],np.array(me5cff.site_terms)[usta_ind],yerr=np.array(me5cff.site_stderr)[usta_ind],fmt='D',markersize=8,color='#796899',elinewidth=2,capsize=4,capthick=1.5,label='Mixed 5 coefficients')
 
 #m e5cff iterative
-plt.errorbar(me5cff_iter.stnum[usta_ind],np.array(me5cff_iter.site_terms)[usta_ind],yerr=np.array(me5cff_iter.site_stderr)[usta_ind],fmt='p',markersize=8,color='#d17c21',elinewidth=2,capsize=4,capthick=1.5,label='Iterative-Mixed 5 coefficients')
+plt.errorbar(me5cff_iter.stnum[usta_ind],np.array(me5cff_iter.site_terms)[usta_ind],yerr=np.array(me5cff_iter.site_stderr)[usta_ind],fmt='p',markersize=8,color='#24822f',elinewidth=2,capsize=4,capthick=1.5,label='Iterative-Mixed 5 coefficients')
 
 
 # Station name text:
@@ -186,6 +196,10 @@ for stationi in range(len(usta)):
 
 
 plt.legend()
+
+plt.xlabel('Station Number and Name')
+plt.ylabel('Site Term (ln residual)')
+
 plt.title('Site vs. Site terms, 5 coefficients/no Vs30 only')
 
 plt.savefig(c5sitefig_png)
@@ -203,7 +217,7 @@ me_iterfig_pdf = pdf_dir + 'me_iter_sites.pdf'
 plt.errorbar(me6cff_iter.stnum[usta_ind],np.array(me6cff_iter.site_terms)[usta_ind],yerr=np.array(me6cff_iter.site_stderr)[usta_ind],fmt='*',markersize=8,color='#dbb36d',elinewidth=2,capsize=6,capthick=1.5,label='Iterative-Mixed 6 coefficients')
 
 # me5cff iterative
-plt.errorbar(me5cff_iter.stnum[usta_ind],np.array(me5cff_iter.site_terms)[usta_ind],yerr=np.array(me5cff_iter.site_stderr)[usta_ind],fmt='p',markersize=8,color='#d17c21',elinewidth=2,capsize=6,capthick=1.5,label='Iterative-Mixed 5 coefficients')
+plt.errorbar(me5cff_iter.stnum[usta_ind],np.array(me5cff_iter.site_terms)[usta_ind],yerr=np.array(me5cff_iter.site_stderr)[usta_ind],fmt='p',markersize=8,color='#24822f',elinewidth=2,capsize=6,capthick=1.5,label='Iterative-Mixed 5 coefficients')
 
 
 # Station name text:
@@ -222,12 +236,255 @@ plt.plot(me6cff_iter.stnum[usta_ind],(me6cff_iter.site_terms[usta_ind] - me5cff_
 plt.legend(loc=2)
 
 plt.ylim([-2.2,2.5])
+
+plt.xlabel('Station Number and Name')
+plt.ylabel('Site Term (ln residual)')
 plt.title('Site vs. Site term, comparison between iterative ME inversions with 6 and 5 coefficients (Vs30/no Vs30')
 
 plt.savefig(me_iterfig_png)
 plt.savefig(me_iterfig_pdf)
 
 
+
+######################
+# Inititate first:
+smfig = plt.figure(figsize=(25,8))
+smfig_png = png_dir + 'sm_sites.png'
+smfig_pdf = pdf_dir + 'sm_sites.pdf'
+
+
+# sm6cff
+plt.errorbar(sm6cff.stnum[usta_ind],np.array(sm6cff.site_terms)[usta_ind],yerr=np.array(sm6cff.site_stderr)[usta_ind],markersize=5,capsize=2,capthick=0.75,fmt='o',color='#687da0',elinewidth=0.75,label='Single-mean 6 coefficients')
+
+# sm5cff 
+plt.errorbar(sm5cff.stnum[usta_ind],np.array(sm5cff.site_terms)[usta_ind],yerr=np.array(sm5cff.site_stderr)[usta_ind],markersize=5,fmt='h',color='#bc0303',elinewidth=0.75,capsize=2,capthick=0.75,label='Single-mean 5 coefficients')
+
+
+# Station name text:
+for stationi in range(len(usta)):
+    statext = sm6cff.sta[usta_ind][stationi]
+    stx = sm6cff.stnum[usta_ind][stationi]
+    sty = -3
+    rotangle = 90
+    
+    plt.text(stx,sty,statext,rotation=rotangle,ha='center',va='bottom')
+    
+plt.plot(sm6cff.stnum[usta_ind],(np.array(sm6cff.site_terms)[usta_ind] - np.array(sm5cff.site_terms)[usta_ind]),linestyle='-.',color='#7c797a',label='Residual')
+plt.plot(sm6cff.stnum[usta_ind],sm6cff.vs30[usta_ind]/760,linestyle=':',color='#448b8c',label='Vs30/Vref')
+plt.plot(sm6cff.stnum[usta_ind],(np.array(sm6cff.site_terms)[usta_ind] - np.array(sm5cff.site_terms)[usta_ind])/(sm6cff.vs30[usta_ind]/760),linestyle='--',color='#8c445b',label='Residual/Vs30/Vref')
+
+plt.legend(loc=2)
+
+plt.ylim([-3.2,5])
+
+plt.xlabel('Station Number and Name')
+plt.ylabel('Site Term (ln residual)')
+plt.title('Site vs. Site term, comparison between single-mean inversions with 6 and 5 coefficients (Vs30/no Vs30')
+
+plt.savefig(smfig_png)
+plt.savefig(smfig_pdf)
+
+
+
+################################################################
+# Sites vs. vs30....
+
+vs30fig = plt.figure(figsize=(10,8))
+vs30png = png_dir + 'sites_vs30.png'
+vs30pdf = pdf_dir + 'sites_vs30.pdf'
+
+# Get correlation values
+me6corr,me6r=pearsonr(me6cff_iter.vs30[usta_ind],np.array(me6cff_iter.site_terms)[usta_ind])
+me5corr,me5r=pearsonr(me5cff_iter.vs30[usta_ind],np.array(me5cff_iter.site_terms)[usta_ind])
+
+# sort by vs30:
+vs30sort = np.argsort(me6cff_iter.vs30[usta_ind])
+
+# Get difference to plot:
+me_site_diff_unsort = np.array(me6cff_iter.site_terms)[usta_ind] - np.array(me5cff_iter.site_terms)[usta_ind]
+me_site_diff = me_site_diff_unsort[vs30sort]
+
+# Plot..
+plt.scatter(me6cff_iter.vs30[usta_ind],np.array(me6cff_iter.site_terms)[usta_ind],s=25,edgecolor='#5d9fba',facecolor='none',marker='D',linewidth=2,label='Mixed iterative, with Vs30, coeff = %.2f, r = %.2f' % (me6corr,me6r)) 
+plt.scatter(me5cff_iter.vs30[usta_ind],np.array(me5cff_iter.site_terms)[usta_ind],s=25,edgecolor='#b57955',facecolor='none',marker='o',linewidth=2,label='Mixed iterative, without Vs30, coeff = %.2f, r = %.2f' % (me5corr, me5r)) 
+
+plt.plot(me6cff_iter.vs30[usta_ind][vs30sort], me_site_diff,linestyle='--',linewidth=2,color='#4f4f4f',label='Residual (vs30 - no vs30)')
+
+plt.ylim([-2,3.5])
+
+plt.xlabel('Vs30 (m/s)')
+plt.ylabel('Site Term (ln residual)')
+plt.title('Site term vs. Vs30')
+
+plt.legend(loc=2)
+
+plt.savefig(vs30png)
+plt.savefig(vs30pdf)
+
+
+
+################################################################
+## Events...
+################################################################
+
+# Get unique events:
+uev,uevind = np.unique(sm6cff.evnum,return_index=True)
+
+
+########
+# All...
+
+# Plot unique events:
+alleventsfig = plt.figure(figsize=(25,8))
+allevfig_png = png_dir + 'all_events.png'
+allevfig_pdf = pdf_dir + 'all_events.pdf'
+
+# Decimate:
+decind = np.arange(0,len(uevind),dfactor)
+
+
+
+# Plot
+plt.errorbar(sm6cff.mw[uevind][decind],np.array(sm6cff.E_residual)[uevind][decind],yerr=np.array(sm6cff.E_stderr)[uevind][decind],markersize=5,fmt='o',color='#687da0',elinewidth=2,capsize=4,capthick=1.5,label='Single-mean 6 coefficients')
+plt.errorbar(me6cff.mw[uevind][decind],np.array(me6cff.E_residual)[uevind][decind],yerr=np.array(me6cff.E_stderr)[uevind][decind],markersize=5,fmt='^',color='#4b7763',elinewidth=2,capsize=4,capthick=1.5,label='Mixed 6 coefficients')
+plt.errorbar(me6cff_iter.mw[uevind][decind],np.array(me6cff_iter.E_residual)[uevind][decind],yerr=np.array(me6cff_iter.E_stderr)[uevind][decind],markersize=5,fmt='*',color='#dbb36d',elinewidth=2,capsize=4,capthick=1.5,label='Mixed iterative 6 coefficients')
+
+plt.errorbar(sm5cff.mw[uevind][decind],np.array(sm5cff.E_residual)[uevind][decind],yerr=np.array(sm5cff.E_stderr)[uevind][decind],markersize=5,fmt='h',color='#bc0303',elinewidth=2,capsize=4,capthick=1.5,label='Single-mean 5 coefficients')
+plt.errorbar(me5cff.mw[uevind][decind],np.array(me5cff.E_residual)[uevind][decind],yerr=np.array(me5cff.E_stderr)[uevind][decind],markersize=5,fmt='D',color='#796899',elinewidth=2,capsize=4,capthick=1.5,label='Mixed 5 coefficients')
+plt.errorbar(me5cff_iter.mw[uevind][decind],np.array(me5cff_iter.E_residual)[uevind][decind],yerr=np.array(me5cff_iter.E_stderr)[uevind][decind],markersize=5,fmt='p',color='#24822f',elinewidth=2,capsize=4,capthick=1.5,label='Mixed iterative 5 coefficients')
+
+plt.legend()
+
+plt.xlabel('Magnitude')
+plt.ylabel('Event Term')
+plt.title('Event terms')
+
+plt.savefig(allevfig_png)
+plt.savefig(allevfig_pdf)
+
+
+
+########
+# 6 coefficients
+
+# Plot unique events:
+cff6eventsfig = plt.figure(figsize=(25,8))
+cff6evfig_png = png_dir + 'cff6_events.png'
+cff6evfig_pdf = pdf_dir + 'cff6_events.pdf'
+
+# Decimate:
+decind = np.arange(0,len(uevind),dfactor)
+
+
+
+# Plot
+plt.errorbar(sm6cff.mw[uevind][decind],np.array(sm6cff.E_residual)[uevind][decind],yerr=np.array(sm6cff.E_stderr)[uevind][decind],markersize=5,fmt='o',color='#687da0',elinewidth=2,capsize=4,capthick=1.5,label='Single-mean 6 coefficients')
+plt.errorbar(me6cff.mw[uevind][decind],np.array(me6cff.E_residual)[uevind][decind],yerr=np.array(me6cff.E_stderr)[uevind][decind],markersize=5,fmt='^',color='#4b7763',elinewidth=2,capsize=4,capthick=1.5,label='Mixed 6 coefficients')
+plt.errorbar(me6cff_iter.mw[uevind][decind],np.array(me6cff_iter.E_residual)[uevind][decind],yerr=np.array(me6cff_iter.E_stderr)[uevind][decind],markersize=5,fmt='*',color='#dbb36d',elinewidth=2,capsize=4,capthick=1.5,label='Mixed iterative 6 coefficients')
+
+plt.legend()
+
+plt.xlabel('Magnitude')
+plt.ylabel('Event Term')
+plt.title('Event terms')
+
+plt.savefig(cff6evfig_png)
+plt.savefig(cff6evfig_pdf)
+
+
+########
+## 5 coefficients
+
+# Plot unique events:
+cff5eventsfig = plt.figure(figsize=(25,8))
+cff5evfig_png = png_dir + 'cff5_events.png'
+cff5evfig_pdf = pdf_dir + 'cff5_events.pdf'
+
+# Decimate:
+decind = np.arange(0,len(uevind),dfactor)
+
+# Plot
+plt.errorbar(sm5cff.mw[uevind][decind],np.array(sm5cff.E_residual)[uevind][decind],yerr=np.array(sm5cff.E_stderr)[uevind][decind],markersize=5,fmt='h',color='#bc0303',elinewidth=2,capsize=4,capthick=1.5,label='Single-mean 5 coefficients')
+plt.errorbar(me5cff.mw[uevind][decind],np.array(me5cff.E_residual)[uevind][decind],yerr=np.array(me5cff.E_stderr)[uevind][decind],markersize=5,fmt='D',color='#796899',elinewidth=2,capsize=4,capthick=1.5,label='Mixed 5 coefficients')
+plt.errorbar(me5cff_iter.mw[uevind][decind],np.array(me5cff_iter.E_residual)[uevind][decind],yerr=np.array(me5cff_iter.E_stderr)[uevind][decind],markersize=5,fmt='p',color='#24822f',elinewidth=2,capsize=4,capthick=1.5,label='Mixed iterative 5 coefficients')
+
+plt.legend()
+
+plt.xlabel('Magnitude')
+plt.ylabel('Event Term')
+plt.title('Event terms')
+
+plt.savefig(cff5evfig_png)
+plt.savefig(cff5evfig_pdf)
+
+
+############
+# SM only
+
+# Plot unique events:
+smeventsfig = plt.figure(figsize=(25,8))
+smevfig_png = png_dir + 'sm_events.png'
+smevfig_pdf = pdf_dir + 'sm_events.pdf'
+
+# Decimate:
+decind = np.arange(0,len(uevind),dfactor)
+
+
+
+# Plot
+plt.errorbar(sm6cff.mw[uevind][decind],np.array(sm6cff.E_residual)[uevind][decind],yerr=np.array(sm6cff.E_stderr)[uevind][decind],markersize=5,fmt='o',color='#687da0',elinewidth=2,capsize=4,capthick=1.5,label='Single-mean 6 coefficients')
+plt.errorbar(sm5cff.mw[uevind][decind],np.array(sm5cff.E_residual)[uevind][decind],yerr=np.array(sm5cff.E_stderr)[uevind][decind],markersize=5,fmt='h',color='#bc0303',elinewidth=2,capsize=4,capthick=1.5,label='Single-mean 5 coefficients')
+
+plt.legend()
+
+plt.xlabel('Magnitude')
+plt.ylabel('Event Term')
+plt.title('Event terms')
+
+plt.savefig(smevfig_png)
+plt.savefig(smevfig_pdf)
+
+
+
+########
+# ME iterative
+
+# Plot unique events:
+meeventsfig = plt.figure(figsize=(25,8))
+meevfig_png = png_dir + 'me_events.png'
+meevfig_pdf = pdf_dir + 'me_events.pdf'
+
+# Decimate:
+decind = np.arange(0,len(uevind),dfactor)
+
+# Get the difference vectors to plot...
+diffind = np.argsort(me6cff.mw[uevind][decind])
+
+
+# Plot
+plt.errorbar(me6cff_iter.mw[uevind][decind],np.array(me6cff_iter.E_residual)[uevind][decind],yerr=np.array(me6cff_iter.E_stderr)[uevind][decind],markersize=5,fmt='*',color='#dbb36d',elinewidth=2,capsize=4,capthick=1.5,label='Mixed iterative 6 coefficients')
+plt.errorbar(me5cff_iter.mw[uevind][decind],np.array(me5cff_iter.E_residual)[uevind][decind],yerr=np.array(me5cff_iter.E_stderr)[uevind][decind],markersize=5,fmt='p',color='#24822f',elinewidth=2,capsize=4,capthick=1.5,label='Mixed iterative 5 coefficients')
+
+plt.plot(me6cff.mw[uevind][decind][diffind],(np.array(me6cff.E_residual)[uevind][decind][diffind] - np.array(me5cff.E_residual)[uevind][decind][diffind]),linestyle='-.',color='#7c797a',label='Residual')
+#plt.plot(sm6cff.mw[uevind],sm6cff.vs30[usta_ind]/760,linestyle=':',color='#448b8c',label='Vs30/Vref')
+#plt.plot(sm6cff.mw[uevind],(np.array(sm6cff.site_terms)[usta_ind] - np.array(sm5cff.site_terms)[usta_ind])/(sm6cff.vs30[usta_ind]/760),linestyle='--',color='#8c445b',label='Residual/Vs30/Vref')
+
+
+plt.legend()
+
+plt.xlabel('Magnitude')
+plt.ylabel('Event Term')
+plt.title('Event terms')
+
+plt.savefig(meevfig_png)
+plt.savefig(meevfig_pdf)
+
+
+
+
+
+
+###########################################################################
 
 ## Diff. between path term plots
 

src/inversion.py
@@ -373,13 +373,14 @@ def mixed_effects(codehome,workinghome,dbname,pred_param,mw,rrup,vs30,evnum,sta,
     # INput data - correct by vs30 or don't?  If data_correct is 0 and there is no a6 specified, then do not correc the data at all, just take ln(data):
     if ((data_correct==0) & (a6=='none')):
         pred_param_corrected=np.log(pred_param)
+        print 'data correction is 0, not correcting data by vs30'
     # If a6 is provided, and it is the same as data_correct, then correct the data by the a6 term:
     elif ((a6!='none') & (a6==data_correct)):
-        pred_param_corrected=np.log(pred_param) + a6*vs30term
+        pred_param_corrected=np.log(pred_param) - a6*vs30term
+        print 'corrected data by vs30 term'
     # If a6 is provided and is not 'none', and data_correct is provided and is not equal to a6, we have a problem...
     elif ((a6!='none') & (a6!=data_correct)):
-        print 'WARNING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n  WARNING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1 \n data_correction for vs30 is not the same as the a6 vs30 term provided...not correcting data by a6 at all \n'
-    
+        print 'WARNING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n  WARNING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1 \n data_correction for vs30 is not the same as the a6 vs30 term provided...not correcting data by a6 at all \n'    
     # If it's all the defaults...then data_correct is -0.6, and a6 is 'none', so correct the data by -0.6*vs30term:    
     elif ((data_correct==-0.6) & (a6=='none')):
         pred_param_corrected = np.log(pred_param) - data_correct*vs30term
@@ -413,9 +414,6 @@ def mixed_effects(codehome,workinghome,dbname,pred_param,mw,rrup,vs30,evnum,sta,
     dbdict['evnum'] = evnum
     dbdict['sta'] = sta
     
-    print '\n dbdict is '
-    print dbdict
-    
     # If there were no coefficients provided, shown below, then add those terms to the dict:
     if a2=='none':
         dbdict['m'] = mw
@@ -441,6 +439,9 @@ def mixed_effects(codehome,workinghome,dbname,pred_param,mw,rrup,vs30,evnum,sta,
     
     # Make datafram ewith Pandas
     data = pd.DataFrame(dbdict)
+    
+    print '\n dbdict is '
+    print dbdict
     
     # Output data to csv:
     csvfile=workinghome+'/models/pckl/'+dbname+'/r/'+dbname+'_mixed.csv'