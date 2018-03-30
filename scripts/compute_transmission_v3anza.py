8f6013cbc6cd0f6aa20e85716247b9ee6e3e2888
vsahakian <sahakian.valerie@gmail.com>
2018-03-30 09:09:52 -0700

    edited output path in wrapper script

--- scripts/compute_transmission_v3anza.py
+++ scripts/compute_transmission_v3anza.py
@@ -42,7 +42,9 @@ coordspath_vp = HOME+'/anza/data/vm/Fang2016/coords.txt'
 materialasciipath_vp = HOME+'/anza/data/vm/Fang2016/Vp.dat'
 
 # Path to output transmission file:
-transmission_path = HOME + '/anza/data/pckl/v3anza_FangVs_transmission.pckl'
+#transmission_path = HOME + '/anza/data/pckl/v3anza_FangVs_transmission.pckl'
+transmission_path = '/home/vsahakian/Desktop/transmission_computation_v3anza/v3anza_FangVs_transmission.pckl'
+
 
 residualpath = HOME + '/anza/models/residuals/mixedregr_v3anza2013_pga_5coeff_a4_-1.20_Mc_8.5_res4_noVs30/mixedcoeff_v3anza2013_pga_noVs30_5coeff_a4_-1.2_pga__ncoeff5_Mc_8.5_VR_99.6_a4_-1.2_robj_FILTERED_raydat.pckl'
 
